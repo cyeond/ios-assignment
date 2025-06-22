eea2b85f52a751a0bd54339fd2a62a961e608690
cyeond <cyeond@gmail.com>
2025-06-22 13:32:54 +0900

    feat: 프로젝트 생성

---
+++ CydAssignment/CydAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CydAssignment
+//
+//  Created by YD on 6/22/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
